9389b55fa8db37a8114ef1ea1c47974f0eca6642
👻👽🤖🧙‍♂️🫠 <sprain6628@gmail.com>
2025-02-02 00:55:17 +0900

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,6 +3,31 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
 
     config = function()
-		vim.api.nvim_set_keymap('n', '<leader>tf', ':Telescope find_files<CR>', { noremap = true, silent = true })
+        local builtin = require('telescope.builtin')
+
+        vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = 'Telescope live grep' })
+        
+        -- 디렉토리 검색
+        vim.api.nvim_set_keymap('n', '<leader>td', '', {
+            noremap = true,
+            silent = true,
+            callback = function()
+                builtin.find_files {
+                    find_command = { "fd", "--type", "d", "--hidden", "--no-ignore"}
+                }
+            end
+        })
+
+        -- 파일, 디렉토리 검색
+        vim.api.nvim_set_keymap('n', '<leader>tt', '', {
+            noremap = true,
+            silent = true,
+            callback = function()
+                builtin.find_files {
+                    find_command = { "fd", "--type", "f", "--type", "d", "--hidden", "--no-ignore"}
+                }
+            end
+        })
     end
 }
