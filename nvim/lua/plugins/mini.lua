f43172a22102ae5b2db001799fd1c2bdbd721512
authentic0376 <150767859+authentic0376@users.noreply.github.com>
2025-10-10 22:12:30 +0900

    Update mini.lua

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,7 +5,10 @@ return {
     config = function()
       -- 이 안에 사용하고 싶은 mini 모듈들의 setup 코드를 넣습니다.
       require('mini.surround').setup()
+
       require('mini.jump2d').setup()
+      vim.api.nvim_set_hl(0, 'MiniJump2dSpot', { reverse = true })      
+
       require('mini.pairs').setup()
       require('mini.move').setup()
       require('mini.comment').setup()
