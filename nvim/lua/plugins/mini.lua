d5548024aff591801c0fece19378b10d93e56a94
sprain <sprain6628@gmail.com>
2025-10-13 03:32:52 +0900

    mini.map 플러그인 추가

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -429,6 +429,15 @@ return {
       -- [,]i 로 이동도 가능
       require("mini.indentscope").setup()
 
+      -- 사이드바 코드 요약
+      require("mini.map").setup()
+      vim.keymap.set("n", "<Leader>mc", MiniMap.close)
+      vim.keymap.set("n", "<Leader>mf", MiniMap.toggle_focus)
+      vim.keymap.set("n", "<Leader>mo", MiniMap.open)
+      vim.keymap.set("n", "<Leader>mr", MiniMap.refresh)
+      vim.keymap.set("n", "<Leader>ms", MiniMap.toggle_side)
+      vim.keymap.set("n", "<Leader>mt", MiniMap.toggle)
+
       -- 오른쪽 상단에 알림창
       require("mini.notify").setup()
 
